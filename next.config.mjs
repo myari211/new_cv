c8ae6f118ad3c2560ad31d99e45887f273ef21e8
myari211 <apputra16@gmail.com>
2025-05-15 18:03:17 +0700

    repair

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: '/new_cv',
+    assetPrefix: '/new_cv/',
+    images: {
+        unoptimized: true,
+    },
+};
 
 export default nextConfig;
